7583e4cc31231dcdc55fd12b766a75b3728a369e
Carlos <solrak29@yahoo.com>
2020-08-05 21:10:39 -0400

    commiting latest changes

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -8,8 +8,37 @@ window.onload = () => {
 }
 
 const getPosts = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((response) => {
+        console.log('received posts');
+        return response.json();
+    }).then((data) => {
+        console.log('build posts');
+        buildPosts(data);
+    });
 }
 
+
 const buildPosts = (blogPosts) => {
+    let blogPostContent = "";
+    for(blogPosts of blogPosts){
+        const postDate = new Date(parseInt(blogPosts.added_date)).toDateString();
+        const postImage = `${API_BASE_URL}${blogPosts.post_image}`;
+        blogPostContent += `
+        <div class="post">
+             <div class="post-image" style="background-image: url(${postImage}">
+             </div>
+             <div class="post-content">
+                 <div class="post-date">${postDate}</div>
+                 <div class="post-title"><h4>${blogPosts.title}</h4></div>
+                 <div class="post-text">
+                     ${blogPosts.content}
+                 </div>
+             </div>
+        </div>
+        `
+        document.querySelector('.blog-post').innerHTML = blogPostContent;
+    }
 
 }
